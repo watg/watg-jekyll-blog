fc18b01ce6663e6d6b742c19e80bfe4c7252e9a0
Rosie-Brigham <rosiebrigham@gmail.com>
2015-12-18 14:01:32 +0000

    adds footer and hides search bar

--- javascripts/core.js
+++ javascripts/core.js
@@ -1,8 +1,11 @@
 blogCore = {}
 var NavbarContent = ""; // navbar global object
+var FooterContent = ""; // footer global object
+
 
 $( document ).ready(function() {
   blogCore.readyNavbar();
+  // blogCore.readyFooter();
 });
 
 
@@ -18,20 +21,21 @@ blogCore.readyNavbar = function() {
 };
 
 blogCore.fetchNavBarContent = function() {
-  var link = document.querySelector('link[rel="import"]');
+  var link = document.querySelector('link[class="watg-header"]');
   var content = link.import;
   NavbarContent = content
 }
 
 // this takes HTML and slaps it into the nav bar bit of the page
 blogCore.readyNavBar = function(content) {
-  var el = content.documentElement.querySelector('.thingie')
+  var el = content.documentElement.querySelector('.watg-header')
   var header = document.getElementById("site-header")
   header.appendChild(el.cloneNode(true));
 
   // then we go and correct those local links and images ;)
   blogCore.correctNavLinks()
   blogCore.correctNavImages()
+  blogCore.hideSearchBar()
 }
 
 blogCore.correctNavLinks = function() {
@@ -44,12 +48,49 @@ blogCore.correctNavLinks = function() {
 }
 
 blogCore.correctNavImages = function() {
-  // This corrects all the local image sources within the nav to ensure it links back to the site
-  var images = $('#site-header').find('img')
-  images.each(function() {
-    var link = $(this).attr('src');
-    $(this).attr("src", "http://d2pnxtyo6k0hqt.cloudfront.net" + link)
-  });
+  var logo = $('#site-header').find('.logo')
+  logo.attr("src", "//www.woolandthegang.com/assets/logo-watg-135x99-0559bf40004567368a1b80fdf79e96b9.png")
+}
+
+blogCore.hideSearchBar = function(){
+  var search = $('#site-header').find('.search-form')
+  search.css("display", "none")
 }
 
 //  ------------------------- Nav Bar Ends ----------------------------------------
+
+//  ------------------------- Footer Starts ---------------------------------------
+
+blogCore.readyFooter = function() {
+  if (FooterContent === "") {
+    blogCore.fetchFooterContent();
+  };
+  blogCore.readyFooterContent(FooterContent)
+};
+
+blogCore.fetchFooterContent = function() {
+  var link = document.querySelector('link[class="watg-footer"]');
+  var content = link.import;
+  FooterContent = content
+}
+
+// // this takes HTML and slaps it into the nav bar bit of the page
+blogCore.readyFooterContent = function(content) {
+  var el = content.documentElement.querySelector('.watg-footer')
+  var footer = document.getElementById("site-footer")
+  footer.appendChild(el.cloneNode(true));
+
+  // then we go and correct those local links and images ;)
+  blogCore.correctFooterLinks()
+}
+
+blogCore.correctFooterLinks = function() {
+  // This corrects all the local URLs within the nav to ensure it links back to the site
+  var anchors = $('#site-footer').find('a')
+  anchors.each(function() {
+    var link = $(this).attr('href');
+    $(this).attr("href", "//www.woolandthegang.com" + link)
+  });
+}
+
+//  ------------------------- Footer Ends -----------------------------------------
